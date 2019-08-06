2b34cdf488cc03a48a78814aa8dafa33eb9e6be3
Marcefv <marcefv.89@gmail.com>
2019-08-05 18:18:57 -0600

    graph

---
+++ ProyectoFinanciera/Controllers/IndicadoresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinanciera.Models;
+
+namespace ProyectoFinanciera.Controllers
+{
+    public class IndicadoresController : Controller
+    {
+        private IndicadoresEntities db = new IndicadoresEntities();
+
+        // GET: Indicadores
+        public ActionResult Index()
+        {
+            return View(db.Indicadores.ToList());
+        }
+
+        // GET: Indicadores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Indicadores indicadores = db.Indicadores.Find(id);
+            if (indicadores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(indicadores);
+        }
+
+        // GET: Indicadores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Indicadores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,COD_INDICADORINTERNO,DES_FECHA,NUM_VALOR")] Indicadores indicadores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Indicadores.Add(indicadores);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(indicadores);
+        }
+
+        // GET: Indicadores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Indicadores indicadores = db.Indicadores.Find(id);
+            if (indicadores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(indicadores);
+        }
+
+        // POST: Indicadores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,COD_INDICADORINTERNO,DES_FECHA,NUM_VALOR")] Indicadores indicadores)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(indicadores).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(indicadores);
+        }
+
+        // GET: Indicadores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Indicadores indicadores = db.Indicadores.Find(id);
+            if (indicadores == null)
+            {
+                return HttpNotFound();
+            }
+            return View(indicadores);
+        }
+
+        // POST: Indicadores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Indicadores indicadores = db.Indicadores.Find(id);
+            db.Indicadores.Remove(indicadores);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProyectoFinanciera/Controllers/SerializeController.cs
@@ -0,0 +1,75 @@
+﻿using ProyectoFinanciera.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+
+namespace ProyectoFinanciera.Controllers
+{
+    public class SerializeController : Controller
+    {
+        // GET: Serialize
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult BasicaPasiva()
+        {
+            return View();
+        }
+
+        public ActionResult DolarVenta()
+        {
+            return View();
+        }
+
+        public ActionResult DolarCompra()
+        {
+            return View();
+        }
+
+        public ActionResult PoliticaMonetaria()
+        {
+            return View();
+        }
+
+        public ActionResult Graph(string codigo, string Title)
+        {
+            DateTime theDate = DateTime.Now;
+            String ahora = theDate.ToString("dd/MM/yyyy");
+            ViewBag.ahora = ahora;
+            DateTime twoyearsago = theDate.AddYears(-2);
+            String hace2anios = twoyearsago.ToString("dd/MM/yyyy");
+            ViewBag.hace2anios = hace2anios;
+            indicadoresReference.wsindicadoreseconomicos objRef = new indicadoresReference.wsindicadoreseconomicos();
+            DataSet ds = objRef.ObtenerIndicadoresEconomicos(codigo, hace2anios, ahora, "Marcela", "N", "marcefv.89@gmail.com", "C0C81E7IFF");
+            List<Indicadores> indicadores = ds.Tables[0].AsEnumerable().
+                Select(dataRow => new Indicadores()
+                {
+                    COD_INDICADORINTERNO = Convert.ToInt32(dataRow["COD_INDICADORINTERNO"]),
+                    DES_FECHA = Convert.ToDateTime(dataRow["DES_FECHA"]),
+                    NUM_VALOR = Convert.ToDecimal(dataRow["NUM_VALOR"])
+                }).ToList();
+
+            ViewBag.y = new List<decimal>();
+            ViewBag.x = new List<DateTime>();
+
+            foreach (var item in indicadores)
+            {
+                ViewBag.y.Add(item.NUM_VALOR);
+                ViewBag.x.Add(item.DES_FECHA);
+            }
+            ViewBag.x.ToArray();
+            ViewBag.y.ToArray();
+            ViewBag.Title = Title;
+            return View();
+
+        }
+
+     
+    }
+}
